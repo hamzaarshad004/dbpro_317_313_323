0f90d9f88c928db8c51b958e7bad6e9cf09340c0
shoaibkh <shoaibkh587@gmail.com>
2019-05-08 20:22:59 -0700

    Modification

--- LMS/LMS/App_Start/RouteConfig.cs
+++ LMS/LMS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LMS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Student", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
